8a15580888d556d0a1503dec386b90de53a59f01
CheikhMohamedD <cheikhdiack313@outlook.fr>
2022-06-21 16:20:11 +0000

    houseKeeping

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  // a regular expression that detects the files that have spec or test in their names
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 };
