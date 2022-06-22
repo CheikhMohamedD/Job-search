b4998ee2807ea0d4d84e006874069450f0821b10
CheikhMohamedD <cheikhdiack313@outlook.fr>
2022-06-22 18:23:43 +0000

    page brand & custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "Brand-gray-1": "#DADCE0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
     },
   },
   plugins: [],
