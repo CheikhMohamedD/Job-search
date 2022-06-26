a0bdb48d79b65823abe7d976a78c82ea5874b042
CheikhMohamedD <cheikhdiack313@outlook.fr>
2022-06-26 23:45:51 +0000

    nav items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -6,11 +6,33 @@
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
         <!-- le href du <a> est lie au script data() par une directive appelle le binding (v-bind) a mettre avant le href -->
-        <a
-          v-bind:href="url"
-          class="flex items-center h-full text-xl font-bold"
-          >{{ company }}</a
-        >
+        <a :href="url" class="flex items-center h-full text-xl font-bold">{{
+          company
+        }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li class="h-full">
+              <a href="" class="flex items-center h-full py-2.5">Teams</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Locations</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5"
+                >Life at SenClient</a
+              >
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">How we hire</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Students</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -29,7 +51,7 @@ export default {
       //   firstname: "Cheikh Mohamed",
       //   lastname: "Diack",
       // },
-      url: "https://carrers.google.com",
+      url: "https://careers.google.com",
     };
   },
 };
